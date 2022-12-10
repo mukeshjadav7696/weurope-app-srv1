ef0470db3ace9aed349bf8f7f4ab117e123a9687
mukeshjadav7696 <54972805+mukeshjadav7696@users.noreply.github.com>
2022-12-10 17:41:05 +0530

    Coomit_7_Azure_Pipeline

--- index.php
+++ index.php
@@ -1,1 +1,61 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Codespaces
+Marketplace
+Explore
+ 
+@mukeshjadav7696 
+mukeshjadav7696
+/
+weurope-app-srv1
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+weurope-app-srv1
+/
+index.php
+in
+main
+ 
+
+Spaces
+
+2
+
+No wrap
+1
 echo "Hello World Welcome to Azure!";
+2
+​
+@mukeshjadav7696
+Commit changes
+Commit summary
+Create index.php
+Optional extended description
+Add an optional extended description…
+ Commit directly to the main branch.
+ Create a new branch for this commit and start a pull request. Learn more about pull requests.
+ 
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
